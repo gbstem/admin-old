2f93d11284eabed07c5bdad60b40b14edd8afffb
Yuen Ler Chow <lerchow@gmail.com>
2023-03-07 23:57:37 -0500

    admin view classes

--- src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
+++ src/routes/(signedIn)/(emailVerified)/dashboard/+page.svelte
@@ -4,37 +4,104 @@
   import { fade } from 'svelte/transition'
   import { user, db } from '$lib/firebase'
 
-  const asyncData = new Promise((resolve, reject) => {
-    let data = {
-      application: '',
-      group: {}
+  let uid = ''
+
+  let data = {}
+  let spreadsheet = []
+
+  const getClasses = classes => {
+    let data = {}
+    const classIds = []
+    for (const row of classes) {
+      if (row.instructorUid === uid || row.instructor2Uid === uid || row.studentUid === uid) {
+        classIds.push(row.classId)
+      }
     }
-    user.loaded().then(() => {
-      getDoc(doc($db, 'applications', $user.uid)).then(res => {
-        if (res.exists()) {
-          const application = res.data()
-          if (application.accepted) {
-            data.application =
-              'You have been accepted to gbSTEM 2023! We look forward to seeing you.'
-          } else {
-            data.application = application.submitted ? 'Submitted and in review!' : 'In progress.'
+    for (const row of classes) {
+      if (classIds.includes(row.classId)) {
+        if (data[row.classId]) {
+          data[row.classId] = {
+            ...data[row.classId],
+            students: [
+              ...data[row.classId].students,
+              row.studentFirstName + ' ' + row.studentLastName
+            ]
           }
         } else {
-          data.application = 'Not started.'
+          data[row.classId] = {
+            course: row.Course,
+            instructorName: row.instructorName,
+            instructor2Name: row.instructor2Name,
+            location:
+              row.Loc === 'CPL Main Branch0' || row.Loc === 'CPL Main Branch'
+                ? 'CPL Main Branch'
+                : 'Virtual',
+            students: [row.studentFirstName + ' ' + row.studentLastName]
+          }
         }
-        resolve(data)
+      }
+    }
+    return data
+  }
+
+  const asyncData = new Promise((resolve, reject) => {
+    let data = {
+      course: '',
+      instructors: [],
+      students: []
+    }
+    user.loaded().then(() => {
+      // get classes doc
+      getDoc(doc($db, 'classes', 'classes')).then(doc => {
+        if (doc.exists()) {
+          spreadsheet = doc.data().classes
+        }
       })
     })
   })
 </script>
 
-{#await asyncData then data}
-  <div class="grid grid-cols-2" transition:fade|local={{ duration: 150 }}>
+<!-- {#await asyncData then data} -->
+<input
+  type="text"
+  bind:value={uid}
+  style="border : 1px solid black"
+  on:change={() => {
+    data = getClasses(spreadsheet)
+  }}
+/>
+
+<div class="grid grid-cols-2" transition:fade|local={{ duration: 150 }}>
+  {#if data && Object.keys(data).length > 0}
+    {#each Object.entries(data) as [key, singleClass]}
+      <Card>
+        <div class="text-lg font-bold">{singleClass.course}</div>
+        <div class="text-sm text-gray-500">Location</div>
+        <div>{singleClass.location}</div>
+        <div class="text-sm text-gray-500">Instructor(s)</div>
+        <ul class="list-disc list-inside">
+          <li>{singleClass.instructorName}</li>
+          {#if singleClass.instructor2Name !== ''}
+            <li>{singleClass.instructor2Name}</li>
+          {/if}
+        </ul>
+        <div class="text-sm text-gray-500">Students</div>
+        <ul class="list-disc list-inside">
+          {#each singleClass.students as student}
+            <li>{student}</li>
+          {/each}
+        </ul>
+      </Card>
+    {/each}
+  {:else}
     <Card>
-      <svelte:fragment slot="title">Application</svelte:fragment>
-      <div class="text-xl">{data.application}</div>
+      <svelte:fragment slot="title">Class</svelte:fragment>
+      <div class="text-sm text-gray-500">
+        It seems like you're not teaching/enrolled in any class.
+      </div>
     </Card>
-  </div>
-{:catch}
-  <div>Error loading data. Please try again.</div>
-{/await}
+  {/if}
+</div>
+<!-- {:catch} -->
+<!-- <div>Error loading data. Please try again.</div> -->
+<!-- {/await} -->
